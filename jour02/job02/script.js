f797336cb213620308a02cf8e005debb95d6af44
Pawel Barc <pawel.barc@laplateforme.io>
2024-03-26 12:48:25 +0100

    J'ai ajouté le job02

---
+++ jour02/job02/script.js
@@ -0,0 +1,9 @@
+
+function showhide(){
+    let x = document.getElementById("citation");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }else {
+     x.style.display = "none";
+    }
+}    
