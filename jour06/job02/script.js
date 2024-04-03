b14649b6f5e9db32eca80bcddd34cfdd7d6e266f
Pawel Barc <pawel.barc@laplateforme.io>
2024-04-03 14:00:45 +0200

    Les jobs 01,02 et 03

--- jour06/job02/script.js
+++ jour06/job02/script.js
@@ -5,16 +5,9 @@ myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
 
-$(document).ready(function(){
-  $("#rebootButton").click(function(){
-    var quotes = [
-      "I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tannhäuser Gate. All those moments will be lost in time, like tears in rain. Time to die.",
-      "Wake up! Time to die!",
-      "I was quit when I come in here, Bryant. I'm twice as quit now.",
-      "Fiery the angels fell. Deep thunder rolled around their shores... Burning with the fires of Orc."
-    ];
-    var randomIndex = Math.floor(Math.random() * quotes.length);
-    var newQuote = quotes[randomIndex];
-    $("#extendedSense").text(newQuote);
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('rebootButton').addEventListener('click', function() {
+      this.textContent = 'Rebooting...'; // Zmiana tekstu na przycisku po kliknięciu
+      // Tutaj możesz dodać dodatkową logikę, np. wywołać funkcję rebootowania systemu
   });
 });
